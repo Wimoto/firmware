8ba08bbba83c47fd3b5731271e0e4f5ad8554410
lorenz00 <lorenzo.dimagiba@wimoto.com>
2015-06-01 15:47:38 -0400

    Rev 2 Conditional Code Sentry

--- WimotoBle/ble_wimoto_sentry_app/connect.c
+++ WimotoBle/ble_wimoto_sentry_app/connect.c
@@ -60,7 +60,7 @@
 #define MODEL_NUM                            "Wimoto_Sentry"                            /**< Model number. Will be passed to Device Information Service. */
 #define MANUFACTURER_ID                      0x1122334455                               /**< Manufacturer ID, part of System ID. Will be passed to Device Information Service. */
 #define ORG_UNIQUE_ID                        0x667788                                   /**< Organizational Unique ID, part of System ID. Will be passed to Device Information Service. */
-#define FIRMWARE_ID 												 "1.10"
+#define FIRMWARE_ID 												 "1.20"
 
 #define APP_ADV_INTERVAL                     0x808                                      /**< The advertising interval (in units of 0.625 ms. This value corresponds to 25 ms). */
 #define APP_ADV_TIMEOUT_IN_SECONDS           0x0000                                     /**< The advertising timeout in units of seconds. */
@@ -155,6 +155,9 @@ static void movement_init(void);
 static void bas_init(void);
 void data_log_sys_event_handler(uint32_t sys_evt);																			/**<function definition of datalog event handler>*/
 static void advertising_init(void);
+
+static uint8_t															 rev_no;																		/**<Revision number of silicon*/
+
 uint8_t										 var_receive_uuid;    											                  /**< varible for receiving the uuid type>**/
 uint8_t				             curr_pir_presence;                              	            /* water pir value for broadcast*/
 uint32_t                   xyz_coordinates;                                             /*accelerometer value for broadcast*/
@@ -1173,7 +1176,7 @@ static void device_manager_init(void)
 
 /**@brief Radio Notification event handler.
 */
-void radio_active_evt_handler(bool radio_active)
+void radio_active_evt_handler_rev1(bool radio_active)
 {	
 		uint32_t err_code;
 	
@@ -1194,6 +1197,13 @@ void radio_active_evt_handler(bool radio_active)
 		#endif
 }
 
+/**@brief Radio Notification event handler when not using rev 1 silicon
+*/
+void radio_active_evt_handler(bool radio_active)
+{		
+		m_radio_event = radio_active;	
+}
+
 
 /**@brief Function for initializing the Radio Notification events.
 */
@@ -1201,18 +1211,18 @@ static void radio_notification_init(void)
 {
     uint32_t err_code;
 		
-		#ifdef REV_1
+		if (rev_no == 0x01) {
 			err_code = ble_radio_notification_init(NRF_APP_PRIORITY_LOW,
 			NRF_RADIO_NOTIFICATION_DISTANCE_800US,
-			radio_active_evt_handler);
+			radio_active_evt_handler_rev1);
 			APP_ERROR_CHECK(err_code);
-		#endif
-		#ifndef REV_1
+		}
+		else {
 			err_code = ble_radio_notification_init(NRF_APP_PRIORITY_HIGH,
 			NRF_RADIO_NOTIFICATION_DISTANCE_4560US,
 			radio_active_evt_handler);
 			APP_ERROR_CHECK(err_code);
-		#endif
+		}
 }
 
 
@@ -1315,6 +1325,23 @@ void HFCLK_request(void)
 	
 }
 
+/**@brief Get silicon revision
+*/
+void get_die_revision_no(void)
+
+{	
+	/*
+	0x0001  0x01  = rev.1 128k FLASH/ 16k RAM or 256k FLASH/ 16k RAM
+	0x0001  0x04  = rev.2 128k FLASH/ 16k RAM or 256k FLASH/ 16k RAM
+	0x0001  0x07  = rev.3 256k FLASH/ 16k RAM
+	0x0001  0x08  = rev.3 128k FLASH/ 16k RAM
+	0x0001  0x09  = rev.3 256k FLASH/ 32k RAM*/
+	
+  rev_no = (uint8_t) ((*(uint32_t *)0xF0000FE8) & 0x000000F0);
+  rev_no = ((rev_no >> 0x04) & (0x0F));
+
+}
+
 /**@brief Function for application main entry.
 */
 void connectable_mode(void)
@@ -1322,6 +1349,7 @@ void connectable_mode(void)
     uint32_t err_code;
 
     // Initialization.
+		get_die_revision_no();								 	/*Get silicon revision before init*/
     ble_stack_init();
     twi_master_init(); 
     MMA7660_config_standby_and_initialize();
@@ -1338,9 +1366,9 @@ void connectable_mode(void)
     MMA7660_enable_active_mode();
     twi_turn_OFF();
     application_timers_start(); 
-		#ifdef rev_1
+		if(rev_no == 0x01){											/* Activate HFCLK workaround for PAN14 if rev 1 silicon*/
 			HFCLK_request();
-		#endif
+		}
 	
 		WDT_init();
     advertising_start();
